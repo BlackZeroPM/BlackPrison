e77547787fd3c5a766a65ac597142aa3503cec8f
BlackZeroPM <pocketminevnp@gmail.com>
2017-07-15 15:58:54 +0700

    Update Main.php

--- MiningPlus-master/src/Muqsit/Main.php
+++ MiningPlus-master/src/Muqsit/Main.php
@@ -30,9 +30,9 @@ class Main extends PluginBase implements Listener{
 	   if($event->isCancelled()) return;
 	   $name = $event->getPlayer()->getName();
 	   $player = $event->getPlayer();
-	      if(self::$breaks[$name] >= 128){
-	         $event->getPlayer()->sendMessage(TF::YELLOW . "You broke 128 blocks, " . TF::AQUA . "WHOOOO!");
-	         self::giveEffect($player, 3, 100, 5);
+	      if(self::$breaks[$name] >= 100){
+	         $event->getPlayer()->sendMessage(TF::YELLOW . "Bạn đã đập được 100 block " . TF::AQUA . "Đào nhanh trong 1 phút!");
+	         self::giveEffect($player, 3, 60, 5);
 	         self::$breaks[$name] = 0; # Reset the counter, to avoid ^^ spam.
 	      }else{
 	        self::$breaks[$name]++;
